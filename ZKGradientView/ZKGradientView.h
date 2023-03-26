680ac65a209d9a5077b514106f1ede80021095be
Zaighum Ghazali Khan <zaighum.khan@arbisoft.com>
2023-03-26 15:17:33 +0500

    Initial project setup

---
+++ ZKGradientView/ZKGradientView.h
@@ -0,0 +1,18 @@
+//
+//  ZKGradientView.h
+//  ZKGradientView
+//
+//  Created by Zaighum on 3/26/23.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for ZKGradientView.
+FOUNDATION_EXPORT double ZKGradientViewVersionNumber;
+
+//! Project version string for ZKGradientView.
+FOUNDATION_EXPORT const unsigned char ZKGradientViewVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <ZKGradientView/PublicHeader.h>
+
+
---
+++ ZKGradientViewExample/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ZKGradientViewExample
+//
+//  Created by Zaighum on 3/26/23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
